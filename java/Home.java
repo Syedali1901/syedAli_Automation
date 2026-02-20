d442a202b882bec0b6f37be86ea54c821845faf9
Syedali1901 <syedalikp0426@gmail.com>
2026-02-20 09:50:54 +0530

    Updating some inputs

--- java/Home.java
+++ java/Home.java
@@ -2,9 +2,8 @@ class Home
 {
     public static void main (string args[])
     {
-
-        System.out.println("Helloooo world");
-        System.out.println("Helloooo world");
+       System.out.println("Helloooo world");
+       System.out.println("Helloooo syed");
     }
 }
 
